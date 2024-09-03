1c7f6d388a6bd4c867fa95bdec29664c5876e19e
Rik Huygen <rik.huygen@kuleuven.be>
2024-09-03 14:22:04 +0200

    ✨ added --version commandline option

--- src/f_fee_tui/__main__.py
+++ src/f_fee_tui/__main__.py
@@ -1,3 +1,7 @@
+import argparse
+import importlib
+import textwrap
+
 from f_fee_tui.app import FastFEEApp
 
 
@@ -6,5 +10,42 @@ def main():
     app.run()
 
 
+def get_version():
+
+    from importlib.metadata import version, PackageNotFoundError
+
+    try:
+        version = version("f_fee_tui")
+    except PackageNotFoundError as exc:
+        version = None
+
+    return version
+
+
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="A Textual User Interface for monitoring and commanding the F-FEE.",
+        epilog=textwrap.dedent(
+            """
+            Prerequisites:
+
+            * The core services shall be running
+            * The `feesim` shall be running unless you are testing real hardware
+            * The `dpu_cs` shall be running
+            * The `data_dump` shall be running when testing F-CAM
+
+            Additional information:
+
+            * TBW
+
+        """),
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    version = get_version()
+    parser.add_argument('--version', action='version', version=f'f-fee-tui {version=}')
+
+    args = parser.parse_args()
+
     main()
